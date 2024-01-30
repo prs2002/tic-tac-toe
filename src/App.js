5334eb7d3e75788d103cbd384aebb69e7462830a
prs2002 <praneetsuyal+github@gmail.com>
2024-01-31 01:27:28 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Board from './Board';
 
 function App() {
+  const [playerSymbol, setPlayerSymbol] = useState('X');
+  const [winner, setWinner] = useState(null);
+
+  useEffect(() => {
+    console.log("Check winner");
+  }, [playerSymbol]);
+
+  const handleBoxClick = (boxIndex) => {
+    console.log("Box clicked");
+  };
+
   return (
     <div className="App">
-      <h1>Lode ka Code</h1>
+      <h1>TicTacToe Game</h1>
+      <Board onBoxClick={handleBoxClick} />
+      {winner && <p>Winner: {winner}</p>}
     </div>
   );
 }
